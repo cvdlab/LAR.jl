9d910b6c8c44d4610fb5f71ce1167527a60e8bba
Alberto Paoluzzi <apaoluzzi@mac.com>
2016-10-10 06:22:40 +0200

    while debugging biconnected components

--- test/biconnected.jl
+++ test/biconnected.jl
@@ -1,62 +1,71 @@
 #=	Biconnected components of a 1-complex.
 An implementation of the Hopcroft-Tarjan algorithm~\cite{Hopcroft:1973:AEA:362248.362272} for computation of the biconnected components of a graph is given here =#
 
+using LAR
+include("src/inters.jl")
+
 
 # Adjacency lists of 1-complex vertices 
-function vertices2vertices(model)
-    V,EV = model
-    csrEV = larcc.csrCreate(EV)
-    csrVE = larcc.csrTranspose(csrEV)
-    csrVV = larcc.matrixProduct(csrVE,csrEV)    
-    cooVV = csrVV.tocoo()
-    data,rows,cols = AA(list)([cooVV.data, cooVV.row, cooVV.col])
-    triples = zip(data,rows,cols)
-    VV = [[] for k in range(len(V))]
-    for datum,row,col in triples
-        if row != col: VV[col] += [row]
-    end
-    AA(sorted)(VV)
+function vertices2vertices(V,EV)
+	lar = Lar()
+	lar.EV = [EV[:,k] for k=1:size(EV,2)]
+	ev = cellComplex(lar.EV)
+	vv = ev'*ev
+	triples = hcat(findnz(vv)...)'
+	VV = [Int64[] for k=1:size(vv,1)]
+	for k=1:size(triples,2)
+		row,col,datum = triples[:,k]
+		if row != col
+			push!(VV[col],row)
+		end
+	end
+	return VV
 end
 
 # Main procedure for biconnected components 
-function biconnectedComponent(model)
-    W,_ = model
-    V = range(len(W))
+function biconnectedComponent(W,EV)
+    V = 1:length(W)
     count = 0
     stack,out = [],[]
-    visited = [None for v in V]
-    parent = [None for v in V]
-    d = [None for v in V]
-    low = [None for v in V]
-    for u in V: visited[u] = False
-    for u in V: parent[u] = []
-    VV = vertices2vertices(model)
+    visited = [false for v in V]
+    parent = [-1 for v in V]
+    d = [-1 for v in V]
+    low = [-1 for v in V]
+    VV = vertices2vertices(W,EV)
     for u in V
-        if not visited[u]
+        if !visited[u]
             DFV_visit( VV,out,count,visited,parent,d,low,stack, u )
         end
     end
-    W,[component for component in out if len(component) > 1]
+    W,[component for component in out if length(component) > 1]
 end
+
+
 
 # Hopcroft-Tarjan algorithm 
 function DFV_visit( VV,out,count,visited,parent,d,low,stack,u )
-    visited[u] = True
+	#println(">> INIZIO *****")
+    visited[u] = true
     count += 1
     d[u] = count
     low[u] = d[u]
-    for v in VV[u]
-        if not visited[v]
-            stack += [(u,v)]
+    for v in VV[u] 
+    	#println("v = $v")
+        if !visited[v]
+            push!(stack, [(u,v)])   # possible bug: []
+            #println("stack = $stack")
             parent[v] = u
+            #println("out = $out")
             DFV_visit( VV,out,count,visited,parent,d,low,stack, v )
             if low[v] >= d[u]
-                out += [outputComp(stack,u,v)]
+            	#println(">> sono io")
+                push!(out,outputComp(stack,u,v))
+                println("\tout = $out")
             end
             low[u] = min( low[u], low[v] )
         else
-            if not (parent[u]==v) and (d[v] < d[u])
-                stack += [(u,v)]
+            if ! (parent[u]==v) & (d[v] < d[u])
+                push!(stack, [(u,v)])   # possible bug: []
                 low[u] = min( low[u], d[v] )
             end
         end
@@ -66,13 +75,28 @@ end
 # Output of biconnected components 
 function outputComp(stack,u,v)
     out = []
-    while True
-        e = stack.pop()
-        out += [list(e)]
-        if e == (u,v) 
+    while true
+        elem = pop!(stack)
+		#println("stack = $stack")
+		#println(">>> eccomi !!")
+        push!(out, elem)
+        if elem == (u,v) 
+        	#println(">>> break !!")
         	break
         end
     end
-    list(set(AA(tuple)(AA(sorted)(out))))
+    out
+    #list(set(AA(tuple)(AA(sorted)(out))))
 end
+
+
+
+
+lineArray = randomLines(10,1.2);
+V,EV = lines2lar(lineArray);
+viewexploded(V,EV)
+VV = vertices2vertices(V,EV)
+model = V,EV
+
+biconnectedComponent(V,EV)
 
---
+++ test/linegraph.jl
@@ -0,0 +1,20 @@
+using LAR
+include("src/inters.jl")
+
+lineArray = randomLines(50,0.4);
+V,EV = lines2lar(lineArray);
+viewexploded(V,EV)
+
+lar = Lar()
+lar.EV = [EV[:,k] for k=1:size(EV,2)]
+ev = cellComplex(lar.EV)
+ee = ev*ev'
+EE = hcat(collect(findnz(ee))...)'
+EW = Set([sort(EE[1:2,k]) for k=1:size(EE,2) if EE[3,k]==1 & (EE[1,k] != EE[2,k])])
+EW = hcat(collect(EW)...)
+W = [mean([V[:,EV[1,h]] V[:,EV[2,h]]],2) for h=1:size(EV,2)]
+W = hcat(W...)
+
+primal = p.COLOR(p.CYAN)(lar2hpc(V,EV))
+dual = p.COLOR(p.YELLOW)(lar2hpc(W,EW))
+p.VIEW(p.STRUCT([primal,dual]))
