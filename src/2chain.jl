936b12c1ef41d81191b69cc6f0f82f62447a0399
Alberto Paoluzzi <apaoluzzi@mac.com>
2016-10-18 05:42:52 +0200

    sparse matrix VE storing edge -post incidence only

--- src/2chain.jl
+++ src/2chain.jl
@@ -1,18 +1,17 @@
 
-function nextPrev(edges::Array{Int64,1})
-	theNexts,thePrevs = Array{Int64,1}(), Array{Int64,1}()
+function nextEdge(edges::Array{Int64,1})
+	theNexts = Array{Int64,1}()
 	n = length(edges)
 	for k in 1:n
 		push!(theNexts, k==n ? edges[1] : edges[k+1] )
-		push!(thePrevs, k==1 ? edges[n] : edges[k-1] )
 	end
-	theNexts,thePrevs
+	theNexts
 end
 
 # Circular ordering of edges around vertices
 function edgeSlopeOrdering(VE,V)
 	VE_sorted = Array{Array{Int64,1},1}()
-	forwardBackward = Tuple{Array{Int64,1},Array{Int64,1},Array{Int64,1}}[]
+	forward = Tuple{Array{Int64,1},Array{Int64,1}}[]
 	for (v,ve) in enumerate(VE)
 		ve_angle = []
 		if ve != []
@@ -26,21 +25,21 @@ function edgeSlopeOrdering(VE,V)
 		pairs = sort(collect(zip(ve_angle,ve)))
 		sortedEdges = [pair[2] for pair in pairs]
 		push!(VE_sorted, sortedEdges)
-		theNexts,thePrevs = nextPrev(sortedEdges)
-		push!(forwardBackward, (sortedEdges,theNexts,thePrevs))
+		theNexts = nextEdge(sortedEdges)
+		push!(forward, (sortedEdges,theNexts))
 		
 	end
-	forwardBackward
+	forward
 end
 
 function cycleBasis(V,EV)
 	I = Int64[]; 
 	J = Int64[]; 
-	Val = Tuple{Int64,Int64}[] 
+	Val = Int64[]; 
 	for (e,(i,j)) in enumerate(EV)
 		push!(I,i); push!(I,j); 
 		push!(J,e); push!(J,e); 
-		push!(Val,(0,0)); push!(Val,(0,0)); 
+		push!(Val,1); push!(Val,1); 
 	end
 	S = sparse(J,I,Val)
 	rows = rowvals(S) 
@@ -50,10 +49,10 @@ function cycleBasis(V,EV)
 		col = [rows[h] for h in nzrange(S,k)]
 		push!(VE, col)
 	end
-	forwardBackward = edgeSlopeOrdering(VE,V)
-	for (v,(edges,nexts,prevs)) in enumerate(forwardBackward)
+	forward = edgeSlopeOrdering(VE,V)
+	for (v,(edges,nexts)) in enumerate(forward)
 		for (h,e) in enumerate(edges)
-			S[edges[h],v] = (nexts[h],prevs[h])
+			S[edges[h],v] = nexts[h]
 		end
 	end
 	return S
